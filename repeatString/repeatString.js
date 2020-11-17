86e83ee339d6c1408c93c6c8b07684a7f8877bb4
Jason Alvarado <jasonalvarado410@gmail.com>
2020-11-16 17:04:51 -0800

    Completed program to pass tests

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, numberRepeat) {
+    if (string === '' || numberRepeat === 0) return ''
+    if (numberRepeat < 0) return 'ERROR' 
+    let addString = string
+    for (let i = 0; i < numberRepeat - 1; i++) {
+        addString += string
+    }
+    return addString
 }
 
 module.exports = repeatString
