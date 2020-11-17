245d644a07e26018ca8c29baab9afb144c013d4a
Jason Alvarado <jasonalvarado410@gmail.com>
2020-11-17 11:41:39 -0800

    Modified code to pass unit tests

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...optionalNums) {
+    let newArray = [];
+    //go through the original array using foreach
+    //items is the current element in the original array  
+    array.forEach((item) => {
+        if (!optionalNums.includes(item)) {
+            newArray.push(item); //push array[i] to the newArray
+        }
+    });
 
+    return newArray;
 }
 
 module.exports = removeFromArray
