47638c4bca61ba03391efa48b6d2c1b47efa5055
Jason Alvarado <jasonalvarado410@gmail.com>
2020-11-17 11:55:13 -0800

    added code to pass unit tests

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,4 +1,14 @@
-const reverseString = function() {
+const reverseString = function(ogString) {
+    let arrayString = ''
+    for (let i = ogString.length - 1; i >= 0; i--) {
+        arrayString += ogString[i]
+    }
+    return arrayString
+
+    //*another way of doing it (utilizing js functions)
+    let splitString = ogString.split('')
+    let reverseArray = splitString.reverse()
+    let joinString = reverseArray.join()
 
 }
 
