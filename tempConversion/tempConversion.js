a9e6ef17b50b8c621af5f1120c2952531b26a051
Jason Alvarado <jasonalvarado410@gmail.com>
2020-11-17 14:53:16 -0800

    added code to pass unit tests

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  //*fahrenheit to celsius: subtract 32, multiply 5, then divide 9 
+  let calculation = (temp - 32) * 5 / 9;
+  return Math.round(calculation * 10) / 10;
 }
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  //*celsius to fahrenheit 
+  let calculation = (temp * 9 / 5) + 32;
+  return Math.round(calculation * 10) / 10;
 }
 
 module.exports = {
