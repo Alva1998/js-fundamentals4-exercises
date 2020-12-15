d93796ce9ee213d43536da645a1fcdb1e849a7db
Jason Alvarado <jasonalvarado410@gmail.com>
2020-12-14 19:49:28 -0800

    multiple calculate functions for basic operations

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,40 @@
-function add () {
-	
+function add(a,b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract(a,b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum(arr) {
+	if (arr.length === 0) return 0;
+	if (arr.length === 1) return arr[0];
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i];
+	}
+	return sum;
 }
 
-function multiply () {
-	
+function multiply(arr) {
+	let sum = arr[0];
+	for (let i = 1; i < arr.length; i++) {
+		sum *= arr[i];
+	}
+	return sum;
 }
 
-function power() {
-	
+function power(a,b) {
+	return a ** b;
 }
 
-function factorial() {
-	
+function factorial (fact) {
+	if (fact === 0 || fact === 1) return 1;
+	let total = fact;
+	for (let i = fact - 1; i >= 1; i--) {
+		total *=  i;
+	}
+	return total;
 }
 
 module.exports = {
